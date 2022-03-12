2a88a072ab13f1d5bfe62b71868931303a1849b4
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2022-03-11 21:25:38 -0600

    Se Solucionaron task satisfactoriamente

---
+++ autoservicio-P3P1/auto-servicio/src/main/java/gt/edu/umg/progra3/colas/AutoServicio.java
@@ -0,0 +1,37 @@
+package gt.edu.umg.progra3.colas;
+
+import java.util.NoSuchElementException;
+
+public class AutoServicio {
+
+    private Cola cola;
+
+    public AutoServicio(Cola cola) {
+        this.cola = cola;
+    }
+    /**
+     * debe retornar el auto que esta en turno, sin sacarlo de la cola
+     * @param placa
+     * @return
+     */
+    public String ingresarAuto(String placa){
+        cola.enqueue(placa);
+        return null;
+    }
+
+    public String proximoAuto(){
+        return cola.peek();
+    }
+
+    /**
+     * debe retornar el auto a entregar y sacarlo de la cola
+     * @return
+     */
+    public String entregarAuto(){
+        return cola.deque();
+    }
+
+    public int obtenerAutosEnCola(){
+        return cola.size();
+    }
+}
